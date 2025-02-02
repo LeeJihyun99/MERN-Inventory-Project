affec8a08c902ade8039755958e1871d465ce1e3
ljihyun99 <Ji.Lee@campus.lmu.de>
2025-02-02 22:46:02 +0100

    created a project and started with backend: with node js, connection to mongodb

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+export const mongoDBURL =
+  "mongodb+srv://root:root@inventory-app-mern.uyom6.mongodb.net/inventory?retryWrites=true&w=majority&appName=inventory-app-mern";
